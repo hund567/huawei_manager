59c420859d6db3a8c8e1b7b234eb4fe1a085aa33
cong guan <hund567@163.com>
2018-03-03 00:41:39 +0800

    first modify

--- testxls.py
+++ testxls.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import requests
 import urllib, urllib2, json
 import cookielib
@@ -5,9 +6,14 @@ import ssl
 import xlwt
 import xlrd
 
-def write_excel()
-    wk = xlwt.Workbook.
-
-
+def write_excel():
+    wk = xlwt.Workbook()
+    st = wk.add_sheet('sheetname', cell_overwrite_ok=True)
+    row0 = [u'主机名', u'主机IP', u'主机wwn', u'lun名称', u'lun_ID', u'lun 大小', u'lun_wwn']
+    for i in range(0, len(row0)):
+        st.write(0, i, row0[i])
+    wk.save('demo3.xls')
+    print('a')
+    return
 if __name__ == '__main__':
     write_excel()
